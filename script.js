352c971ce3553c44078825c32accab09eca5a901
Jenny Sila <jennysila@Jennys-MacBook-Pro.local>
2019-05-26 13:05:04 +0200

    burgermenu påbegyndt

--- script.js
+++ script.js
@@ -8,16 +8,22 @@ function sidenVises() {
 
 }
 
-
 function toggleMenu() {
     console.log("toggleMenu");
+
+    //hvordan er toggle ift. add og remove??? HELP
     document.querySelector("#menu").classList.toggle("hidden");
 
+    // let erskjult er lig med navmuligheder når den indeholder class'en hidden
     let erSkjult = document.querySelector("#menu").classList.contains("hidden");
-
+    // hvis navmuligheder er skjult, så skal man se en burgermenu
     if (erSkjult == true) {
-        document.querySelector("#menuknap").textContent = "☰";
-    } else {
-        document.querySelector("#menuknap").textContent = "X";
+        document.getElementById("menuknap_img").src = "startside_ui/Menu.svg";
+        //document.querySelector("#menuknap").textContent = "☰";
+    }
+
+    //hvis i navmuligheder er skjult skal man se et kryds
+    else {
+        document.getElementById("menuknap_img").src = "startside_ui/No.svg";
     }
 }
